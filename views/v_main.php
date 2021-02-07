e2940a8373fea14f28305ed32019454423d90072
Orioru <omart1881@gmail.com>
2021-02-07 12:38:34 +0100

    Incluido bootstrap y pequeño cambio en el main

--- views/v_main.php
+++ views/v_main.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>SPE</h1>
+<div class="main-container p-5 border">
+    <h1>Seguiment de les Pràctiques Externes</h1>
         <table>
             <thead>
                 <tr>
@@ -27,5 +31,6 @@
                 ?>
             </tbody>
         </table>
+    </div>
 </body>
 </html>
